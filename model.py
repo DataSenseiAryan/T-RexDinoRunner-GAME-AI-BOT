4e638fabdb168748a145884dfaf16ce78ca31d87
aryanc55 <aryanc55@gmail.com>
2019-07-24 01:22:16 +0530

    pushed whole code

---
+++ model.py
@@ -0,0 +1,444 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import numpy as np
+from PIL import Image
+import cv2 #opencv
+import sys 
+import io
+import time
+import pandas as pd
+import numpy as np
+from IPython.display import clear_output
+from random import randint
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+
+import random
+import pickle
+from io import BytesIO
+import base64
+import json
+
+
+########################################################
+#path variables
+game_url = "chrome://dino"
+chrome_driver_path = "/usr/local/bin/chromedriver"
+
+#scripts
+#create id for canvas for faster selection from DOM
+init_script = "document.getElementsByClassName('runner-canvas')[0].id = 'runner-canvas'"
+
+#get image from canvas
+getbase64Script = "canvasRunner = document.getElementById('runner-canvas'); \
+return canvasRunner.toDataURL().substring(22)"
+
+
+
+#######GAME STATE######
+'''
+* Game class: Selenium interfacing between the python and browser
+* __init__():  Launch the broswer window using the attributes in chrome_options
+* get_crashed() : return true if the agent as crashed on an obstacles. Gets javascript variable from game 
+                  decribing the state
+* get_playing(): true if game in progress, false is crashed or paused
+* restart() : sends a signal to browser-javascript to restart the game
+* press_up(): sends a single to press up get to the browser
+* get_score(): gets current game score from javascript variables.
+* pause(): pause the game
+* resume(): resume a paused game if not crashed
+* end(): close the browser and end the game
+'''
+class Game:
+    def __init__(self,custom_config=True):
+        chrome_options = Options()
+        chrome_options.add_argument("disable-infobars")
+        chrome_options.add_argument("--mute-audio")
+        self._driver = webdriver.Chrome(executable_path = chrome_driver_path,chrome_options=chrome_options)
+        self._driver.set_window_position(x=-10,y=0)
+        self._driver.get('chrome://dino')
+        self._driver.execute_script("Runner.config.ACCELERATION=0")
+        self._driver.execute_script(init_script)
+    def get_crashed(self):
+        return self._driver.execute_script("return Runner.instance_.crashed")
+    def get_playing(self):
+        return self._driver.execute_script("return Runner.instance_.playing")
+    def restart(self):
+        self._driver.execute_script("Runner.instance_.restart()")
+    def press_up(self):
+        self._driver.find_element_by_tag_name("body").send_keys(Keys.ARROW_UP)
+    def get_score(self):
+        score_array = self._driver.execute_script("return Runner.instance_.distanceMeter.digits")
+        score = ''.join(score_array) # the javascript object is of type array with score in the formate[1,0,0] which is 100.
+        return int(score)
+    def pause(self):
+        return self._driver.execute_script("return Runner.instance_.stop()")
+    def resume(self):
+        return self._driver.execute_script("return Runner.instance_.play()")
+    def end(self):
+        self._driver.close()
+
+
+
+class DinoAgent:
+    def __init__(self,game): #takes game as input for taking actions
+        self._game = game; 
+        self.jump(); #to start the game, we need to jump once
+    def is_running(self):
+        return self._game.get_playing()
+    def is_crashed(self):
+        return self._game.get_crashed()
+    def jump(self):
+        self._game.press_up()
+    def duck(self):
+        self._game.press_down()
+
+
+
+
+
+class Game_state:
+    def __init__(self,agent,game):
+        self._agent = agent
+        self._game = game
+        self._display = show_img() #display the processed image on screen using openCV, implemented using python coroutine 
+        self._display.next() # initiliaze the display coroutine 
+    def get_state(self,actions):
+        #actions_df.loc[len(actions_df)] = actions[1] # storing actions in a dataframe
+        score = self._game.get_score() 
+        reward = 0.1
+        is_over = False #game over
+        if actions[1] == 1:
+            self._agent.jump()
+
+        image = grab_screen(self._game._driver) 
+        self._display.send(image) #display the image on screen
+        if self._agent.is_crashed():
+            #scores_df.loc[len(loss_df)] = score # log the score when game is over
+            self._game.restart()
+            reward = -1
+            is_over = True
+        image = image_to_tensor(image)
+        
+        return image, reward, is_over #return the Experience tuple
+
+
+
+######### HANDLER_FUNCTIONS ###########
+
+def grab_screen(_driver):
+    image_b64 = _driver.execute_script(getbase64Script)
+    screen = np.array(Image.open(BytesIO(base64.b64decode(image_b64))))
+    image = process_img(screen)#processing image as required
+
+    return image
+
+
+
+
+def process_img(image):
+    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #RGB to Grey Scale
+    image = image[:300, :500] #Crop Region of Interest(ROI)
+    image = cv2.resize(image, (84,84))
+    image[image> 0] = 255
+    image = np.reshape(image, (84,84,1))
+
+        #image = image_to_tensor(image) coomented out due to  cv2.error: OpenCV(3.4.1)/io/opencv/modules/imgcodecs/src/utils.cpp:622: error: (-15) Source image must have 1, 3 or 4 channels in function cvConvertImage    now adding this call just before returning in get state 
+    return  image
+
+def image_to_tensor(image):
+
+    image = np.transpose(image, (2, 0, 1))  #84*84*1 to 1*84*84 apply this code
+
+    
+      
+    
+    image_tensor = image.astype(np.float32)
+    
+
+    image_tensor = torch.from_numpy(image_tensor)
+    
+    
+    
+    if torch.cuda.is_available():  # put on GPU if CUDA is available
+        image_tensor = image_tensor.cuda()
+       
+  
+    
+    return image_tensor
+
+
+
+
+def show_img(graphs = False):
+    """
+    Show images in new window
+    """
+    while True:
+        screen = (yield)
+        window_title = "T_REX NIBBA" #"logs" if graphs else 
+        cv2.namedWindow(window_title, cv2.WINDOW_NORMAL)
+        #screen=screen.cpu().numpy() #as now not sending tensor here calling befire converting to tensor as changes made in getsatate fun 
+        imS = cv2.resize(screen, (800, 400)) 
+        cv2.imshow(window_title, screen)
+        if (cv2.waitKey(1) & 0xFF == ord('q')):
+            cv2.destroyAllWindows()
+            break
+
+
+
+########## HANDLER_FUNCTION ###########
+
+
+
+
+########################################################
+
+
+class NeuralNetwork(nn.Module):
+
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+
+        self.number_of_actions = 2
+        self.gamma = 0.99
+        self.final_epsilon = 0.0001
+        self.initial_epsilon = 0.1
+        self.number_of_iterations = 2000000
+        self.replay_memory_size = 10000
+        self.minibatch_size = 32
+
+        self.conv1 = nn.Conv2d(4, 32, 8, 4)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(32, 64, 4, 2)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(64, 64, 3, 1)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.fc4 = nn.Linear(3136, 512)
+        self.relu4 = nn.ReLU(inplace=True)
+        self.fc5 = nn.Linear(512 , self.number_of_actions)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.conv3(out)
+        out = self.relu3(out)
+        out = out.view(out.size()[0], -1)
+        out = self.fc4(out)
+        out = self.relu4(out)
+        out = self.fc5(out)
+
+        return out
+
+
+def init_weights(m):
+    if type(m) == nn.Conv2d or type(m) == nn.Linear:
+        torch.nn.init.uniform(m.weight, -0.01, 0.01)
+        m.bias.data.fill_(0.01)
+
+
+
+
+def train(model, start):
+    # define Adam optimizer
+    optimizer = optim.Adam(model.parameters(), lr=1e-6)
+
+    # initialize mean squared error loss
+    criterion = nn.MSELoss()
+
+    # instantiate game
+    game = Game()
+    dino = DinoAgent(game)
+    game_state = Game_state(dino,game)
+
+    # initialize replay memory
+    replay_memory = []
+
+    # initial action is do nothing
+    action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+    action[0] = 1
+    image_data, reward, terminal = game_state.get_state(action)
+
+    
+   # image_data = resize_and_bgr2gray(image_data) no need as already called in game_state 
+    #image_data = image_to_tensor(image_data) 
+    state = torch.cat((image_data, image_data, image_data, image_data)).unsqueeze(0) #stacking 4 images
+    
+    print("printing size of input state at 0")
+    print(state.size())
+
+    # initialize epsilon value
+    epsilon = model.initial_epsilon
+    iteration = 0
+
+    epsilon_decrements = np.linspace(model.initial_epsilon, model.final_epsilon,model.number_of_iterations)
+
+    # main infinite loop
+    while iteration < model.number_of_iterations:
+        # get output from the neural network
+        output = model(state)[0]
+
+        # initialize action
+        action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            action = action.cuda()
+
+        # epsilon greedy exploration
+        random_action = random.random() <= epsilon
+        if random_action:
+            print("Performed random action!")
+        action_index = [torch.randint(model.number_of_actions, torch.Size([]), dtype=torch.int)
+                        if random_action
+                        else torch.argmax(output)][0]
+
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            action_index = action_index.cuda()
+
+        action[action_index] = 1
+
+        # get next state and reward
+        image_data_1, reward, terminal = game_state.get_state(action)
+        #image_data_1 = resize_and_bgr2gray(image_data_1)
+        #image_data_1 = image_to_tensor(image_data_1)
+        state_1 = torch.cat((state.squeeze(0)[1:, :, :], image_data_1)).unsqueeze(0)
+
+        action = action.unsqueeze(0)
+        reward = torch.from_numpy(np.array([reward], dtype=np.float32)).unsqueeze(0)
+
+        # save transition to replay memory
+        replay_memory.append((state, action, reward, state_1, terminal))
+
+        # if replay memory is full, remove the oldest transition
+        if len(replay_memory) > model.replay_memory_size:
+            replay_memory.pop(0)
+
+        # epsilon annealing
+        epsilon = epsilon_decrements[iteration]
+
+        # sample random minibatch
+        minibatch = random.sample(replay_memory, min(len(replay_memory), model.minibatch_size))
+
+        # unpack minibatch
+        state_batch = torch.cat(tuple(d[0] for d in minibatch))
+        action_batch = torch.cat(tuple(d[1] for d in minibatch))
+        reward_batch = torch.cat(tuple(d[2] for d in minibatch))
+        state_1_batch = torch.cat(tuple(d[3] for d in minibatch))
+
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            state_batch = state_batch.cuda()
+            action_batch = action_batch.cuda()
+            reward_batch = reward_batch.cuda()
+            state_1_batch = state_1_batch.cuda()
+
+        # get output for the next state
+        output_1_batch = model(state_1_batch)
+
+        # set y_j to r_j for terminal state, otherwise to r_j + gamma*max(Q)
+        y_batch = torch.cat(tuple(reward_batch[i] if minibatch[i][4]
+                                  else reward_batch[i] + model.gamma * torch.max(output_1_batch[i])
+                                  for i in range(len(minibatch))))
+
+        # extract Q-value
+        q_value = torch.sum(model(state_batch) * action_batch, dim=1)
+
+        # PyTorch accumulates gradients by default, so they need to be reset in each pass
+        optimizer.zero_grad()
+
+        # returns a new Tensor, detached from the current graph, the result will never require gradient
+        y_batch = y_batch.detach()
+
+        # calculate loss
+        loss = criterion(q_value, y_batch)
+
+        # do backward pass
+        loss.backward()
+        optimizer.step()
+
+        # set state to be state_1
+        state = state_1
+        iteration += 1
+
+        if iteration % 2500 == 0:
+            torch.save(model, "pretrained_model/current_model_" + str(iteration) + ".pth")
+
+        print("iteration:", iteration, "elapsed time:", time.time() - start, "epsilon:", epsilon, "action:",
+              action_index.cpu().detach().numpy(), "reward:", reward.numpy()[0][0], "Q max:",
+              np.max(output.cpu().detach().numpy()))
+
+def test(model):
+    
+    game = Game()
+    dino = DinoAgent(game)
+    game_state = Game_state(dino,game)
+
+    
+    # initial action is do nothing
+    action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+    action[0] = 1
+    image_data, reward, terminal = game_state.get_state(action)
+    state = torch.cat((image_data, image_data, image_data, image_data)).unsqueeze(0)
+
+    while True:
+        # get output from the neural network
+        output = model(state)[0]
+
+        action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            action = action.cuda()
+
+        # get action
+        action_index = torch.argmax(output)
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            action_index = action_index.cuda()
+        action[action_index] = 1
+
+        # get next state
+        image_data_1, reward, terminal = game_state.get_state(action)
+        state_1 = torch.cat((state.squeeze(0)[1:, :, :], image_data_1)).unsqueeze(0)
+
+        # set state to be state_1
+        state = state_1
+
+
+
+def main(mode):
+    cuda_is_available = torch.cuda.is_available()
+
+    if mode == 'test':
+        model = torch.load(
+            'pretrained_model/current_model_40375.pth',
+            map_location='cpu' if not cuda_is_available else None
+        ).eval()
+
+        if cuda_is_available:  # put on GPU if CUDA is available
+            model = model.cuda()
+
+        test(model)
+
+    elif mode == 'train':
+        if not os.path.exists('pretrained_model/'):
+            os.mkdir('pretrained_model/')
+
+        model = NeuralNetwork()
+
+        if cuda_is_available:  # put on GPU if CUDA is available
+            model = model.cuda()
+
+        model.apply(init_weights)
+        start = time.time()
+
+        train(model, start)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
+
